cb22b7246874086c14d512c26c9f8ac9d09ffe26
Administrator <Administrator@192.168.11.43>
2021-12-23 10:26:39 +0900

    1

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -3,10 +3,28 @@ package co.jp.chapter01;
 public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 鑷嫊鐢熸垚銇曘倢銇熴儭銈姐儍銉夈兓銈广偪銉�
 
 
-		// dummy commit
+		// 質問１︓以下変数nの値は︖
+		// n=0;
+		
+		//質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
+		if(a>10) {
+			a=11;
+		}else {
+			a=0;
+		}
+		
+		
+		//質問３︓変数値を⽐較する演算⼦は = or == ?
+		// ==です。
+		
+		//質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		//「true」と「false」です。
+		
+		
+		
 	}
 
 }
