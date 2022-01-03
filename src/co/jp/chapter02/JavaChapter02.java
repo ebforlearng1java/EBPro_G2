d515d13ce7ad55da954088b53c195723f61b582b
BruceTong36 <61258996+BruceTong36@users.noreply.github.com>
2022-01-03 11:22:46 +0900

    董 之立(ドン シリツ) 宿題提出-20220103

--- src/co/jp/chapter02/JavaChapter02.java
+++ src/co/jp/chapter02/JavaChapter02.java
@@ -4,7 +4,16 @@ public class JavaChapter02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+//		String a = "Hello";
+//		String b = "World";
+//		boolean T = a.equals(b);
+//		System.out.println(T);
+//		
+//		System.out.println(1.0+2.0);
+//		System.out.println(1.0+"2.0");
+		
+		int a = 10;
+		System.out.println("a"); 
 
 	}
 
--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,8 +3,32 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
-
-
+//		質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖ 
+//		1. １⾏ 2. ２⾏
+//		答え：1行です。
+		
+//		質問２︓Stringを⽐較⽅法は︖
+//		1. == 2. equals
+//		答え：「equals」です。
+//		例：		
+		String a = "Hello";
+		String b = "World";
+		boolean T = a.equals(b);
+		System.out.println(T);
+		
+//		質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+//		1. String.valueOf(“1.0F“) 2. 1.0F + ““
+//		答え：「String.valueOf(“1.0F“)」です。
+		
+//		質問４︓以下式の値を記載してください。
+//		1. 1.0 + 2.0 2. 1.0 + “2.0“
+//		答え：1．3  
+//		    2．1.02.0
+		
+//		質問５︓以下ステートメントの出⼒を記載してください。 いいね 1 番に「いいね」しましょう ⽂字 説明 \' ' \" “ \\ \ \t Tab \n 改⾏ 1234 public static void main(String[] args) {
+//		int a = 10;
+//		System.out.println("a"); }
+//		答え：「a」です。
 	}
 
 }
