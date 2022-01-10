2a79ab5dc8cd847c458f53c851a6727d4e2364f0
jj <jj@LAPTOP-F41NPBCF>
2022-01-10 18:18:22 +0900

    homework01 20220110 wang

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -3,10 +3,40 @@ package co.jp.chapter01;
 public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-
-		// dummy commit
+		
+		/*質問１︓以下変数nの値は︖
+		 * 1 int i = 10;
+		 * 2 int n = i%5;
+		 */
+		int i = 10;
+		int n = i % 5;
+		System.out.println("質問1変数nの値：" + n);
+		
+		
+		//質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
+		int a = 10;
+		
+		if(a > 10) {
+			a = 11;
+		}else {
+			a = 0;
+		}
+		System.out.println("質問2変数aの値：" + a);
+		
+		
+		//質問３︓変数値を比較する演算⼦は = or == ?
+		int x = 1;
+		
+		if(x == 1) {
+			x = 10;
+		}
+		System.out.println("質問3変数xの値：" + x + "変数値を比較する演算子は ==");
+		
+		
+		//質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		boolean t = true;
+		boolean f = false;
+		System.out.println("質問4ブリアン（ boolean ）変数に設定可能の値は：" + t + "と" + f);
 	}
 
 }
