b914c7d6f03209ee0064188aa0dd1d19855252b5
ho.kokukei <i@tenone.me>
2022-01-10 22:38:21 +0900

    homework01 & homework02

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -5,6 +5,30 @@ public class JavaHomeWork01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+//		質問１︓以下変数nの値は︖
+
+//	    n的值是0
+		
+//		質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0; 
+		
+		int a=0;
+		
+		if (a>10) {
+			a=11;
+		}else {
+			a=0;
+		}
+				
+//		質問３︓変数値を⽐較する演算⼦は = or == ? 
+		
+//		答案是 ==
+				
+//		質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		
+		
+		boolean b=true;
+		b=false;
+		
 
 		// dummy commit
 	}
