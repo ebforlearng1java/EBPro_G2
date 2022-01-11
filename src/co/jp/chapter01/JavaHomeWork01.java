37f9119362c891cc60ebbb0a9acae90486e312a5
mleng <mleng@MSI>
2022-01-11 18:30:59 +0900

    HW 1 and HW2 2022/01/11 Leng Mingbin

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -1,5 +1,7 @@
 package co.jp.chapter01;
 
+import java.util.Scanner;
+
 public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
@@ -7,6 +9,43 @@ public class JavaHomeWork01 {
 
 
 		// dummy commit
+		
+		
+		// 質問１︓以下変数nの値は︖ 
+		//1 int i = 10;
+		//2 int n = i%5;
+				
+		int i = 10;
+		int n = i%5;
+		System.out.println(i);
+		System.out.println(n);
+				
+				
+				
+		// 質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
+				
+		Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+		System.out.print("Enter an integer: ");  
+		int a = sc.nextInt();
+				 
+		if (a > 10) {
+			System.out.println("a = 11");
+					 }
+		else {
+			System.out.println("a = 0");
+			 }
+				
+				
+				
+		// 質問３︓変数値を⽐較する演算⼦は = or == ?
+				
+		// 「==」 です。
+				
+				
+		// 質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		// 「true]「false」です。
+		
+		
 	}
 
 }
--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,29 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
+		
+		// 質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖
+		
+		// 1行です。
+				
+		// 質問２︓Stringを⽐較⽅法は︖
+				
+		// Stringの文字列を比較するため、「equals]を使います。
+				
+		// 質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+		// 「String.valueOf(“1.0F“)」です。
+				
+		// 質問４︓以下式の値を記載してください。
+		// 1. 1.0 + 2.0
+		// 2. 1.0 + “2.0“
+		// 1. 3.0です。
+		// 2.  1.02.0です。
+				
+		// 質問５︓以下ステートメントの出⼒を記載してください。
+		//int a = 10;
+		//System.out.println("a");
+				
+		// 出力は「a」です。
 
 
 	}
