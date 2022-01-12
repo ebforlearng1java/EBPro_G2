a82e98349448c9acd7aa67677f06a89ea23776ee
Senyorum <Senyoruminatsu@users.noreply.github.com>
2022-01-12 23:01:44 +0900

    【homewprk up 1】20220112 WANG LIFENG

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -3,11 +3,108 @@ package co.jp.chapter04;
 public class JavaHomeWork04 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 鑷嫊鐢熸垚銇曘倢銇熴儭銈姐儍銉夈兓銈广偪銉�
+//
+//		System.out.println(String.valueOf("1.0F"));
+//		System.out.println(String.valueOf(1.0F));
+//		System.out.println(1.0F +"");
+		
+		
+		//問題１︓int[] array = new int[4]; 各要素の値を記載してください。
+		//   四つの0； 
+		//array={0,0,0,0};
+		
+		
+		//問題２︓int[] array = new int[] {5, 6}; 各要素の値を記載してください。
 
-		System.out.println(String.valueOf("1.0F"));
-		System.out.println(String.valueOf(1.0F));
-		System.out.println(1.0F +"");
+		// 5と6です。　
+		//array={5,6};
+		
+	
+		
+		
+//		
+//		int[] array = new int[] {5, 6};
+//		for(int i = 0; i < array.length; i++) {
+//			 System.out.println(array[i]); // 配列の要素を出力する
+//			}
+		
+	
+		
+		
+		
+		//問題３︓int[][] array = new int[2][3]; 各要素の値を記載してください。
+		
+		//{0,0}
+		//{0,0}
+		//{0,0}
+		
+		
+		
+		//問題４︓以下２重Foreachを理解してください。
+//		int[][] array2d = new int[4][2];
+//		for (int[] out : array2d) {
+//			 for (int inner : out) {
+//			 System.out.println(inner);
+//			 }
+//			}
+		
+		//先将数列out等同于int[][0];输出第一行的所有值后切换到int[][1]输出所有值
+		//即为逐行逐个输出array2d的所有值
+
+		
+		
+		//問題５︓int[] array = new int[] {1, 3, 5, 7, 100, 0, 1}; 左の配列の最⼤値と最⼩桁値をSystem.out.println()。
+		
+//		int[] array = new int[] {1, 3, 5, 7, 100, 0, 1};
+//		int max=array[0],min=array[0];
+//		for(int i = 1; i < array.length; i++) {
+//			 int x = array[i];
+//			 if(x>max) max=x;
+//			 if(x<min) min=x;
+//			}
+//		System.out.println("最⼤値:"+max);
+//		System.out.println("最⼩値:"+min);
+//		
+
+		//問題６︓問題１の配列のSUMを求める
+//		int sum=0;
+//		for(int x : array) {
+//			 sum+=x;
+//			}
+//		System.out.println(sum);
+		
+		
+		
+		//問題７︓⾏列（２次元配列）の和と積を計算してください。
+//		int[][] array1 = new int[][]{
+//			 {1,2,3},
+//			 {4,5,6},
+//			 {7,8,9},
+//			};
+//		int[][] array2 = new int[][]{
+//				 {1,2,3},
+//				 {4,5,6},
+//				 {7,8,9},
+//				};
+//			
+//
+//		int[][] sum =new int[3][3]; 
+//		int[][] mul =new int[3][3]; 
+//		
+//		for(int i=0;i<3;i++) {
+//			for(int j=0;j<3;j++) {
+//				sum[i][j]=array1[i][j]+array2[i][j];
+//				mul[i][j]=array1[i][j]*array2[i][j];
+//				//System.out.print(mul[i][j]+" ");
+//			}
+//			//System.out.println();
+//		}
+//		
+//		
+//		
+		
+		
 	}
 
 }
