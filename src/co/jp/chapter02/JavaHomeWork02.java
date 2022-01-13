fc85d354286dc2799f36cdc6ad78ba544b5e061e
cyanj_ifn59vk <cyanj_ifn59vk@192.168.10.107>
2022-01-14 01:48:10 +0900

    homework up 20220114 chen

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -5,34 +5,37 @@ public class JavaHomeWork02 {
 	public static void main(String[] args) {
 
 		// TODO 自動生成されたメソッド・スタブ
+		
+		
+//       homework
 		     
 //      質問１︓ System.out.println(“this is line1\tthis line2“); は何行出力しますか︖
 //		1. １⾏
 //		2. ２⾏
-//	      答： １行
+//	      答： １行  
 		
 //	    質問２︓Stringを比較方法は︖
 //		1. ==
 //		2. equals
-//	　　　　　 答：　==
+//	　　　　　 答：　==  
 		
 //      質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
 //		1. String.valueOf(“1.0F“)
 //		2. 1.0F + ““
 //	        答： 1
-		
+		 
 //		質問４︓以下式の値を記載してください。
 //		1. 1.0 + 2.0
-//		2. 1.0 + “2.0“
+//		2. 1.0 + “2.0“ 
 // 		   答：  3
-//		       12
+//		       12 
 		
 //		質問５︓以下ステートメントの出⼒を記載してください。　　　　　
 //      1 public static void main(String[] args) {
-//		2 int a = 10;
-//		3 System.out.println("a");
+//		2 int a = 10;  
+//		3 System.out.println("a"); 
 //		4 }
-//	       答：  10 
+//	       答：  10  
 		
 		
 		
