7b982249d19bd9b6f0d3d4f0404854dd301bb9ad
52430 <52430@LAPTOP-O4VPII06>
2022-01-13 23:51:29 +0900

    assignment

--- src/co/jp/chapter03/JavaHomeWork03.java
+++ src/co/jp/chapter03/JavaHomeWork03.java
@@ -4,7 +4,36 @@ public class JavaHomeWork03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//質問１︓成績ランキング判断を作成する「90以上︓A, 80-90︓B, 70-80︓C, 60-70︓ D, 60以下︓E」
+		//回答：
+		//int a = 60;
+		System.out.println("成績ランキング判断。入力値=" + a);
+		// 以下コードを完成してください。
+		//if(a >= 90) {
+		// System.out.println("A"); } 
+		//else if(a < 90 && a >= 80) {
+		// System.out.println("B"); } 
+		//else if(a < 80 && a >= 70) {
+	//	 System.out.println("C"); } 
+		//else if(a < 70 && a >= 60) {
+		// System.out.println("D"); }
+	//	else {
+		// System.out.println("E");
+		 //質問２︓⽇本のコインを１円、５円、１０円、１００円、５００円があります。３３４０ 円のコイン数をもとめください。
+		 //回答：
+		 int one;
+		 int five;
+		 int ten;
+		 int hundred;
+		 int fivehundred;
+		 int total = 3340;
+		 one + 5*five +
+		 if(one == 0) {
+	      
+		 
+		 
 
 	}
 
+ }
 }
