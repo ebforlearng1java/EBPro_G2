6dbd1f2d13e05373e83fb606780e2bb4845ac83a
Kin <kin.career@outlook.com>
2022-01-14 22:15:44 +0900

    20220114_homework_upload

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -3,13 +3,27 @@ package co.jp.chapter01;
 public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		/*
 
-		int i = 0;
-		// dummy commit
-		// UPLOAD TEST
-		
-	
+		質問１︓以下変数nの値は？
+		int i = 10;
+		int n = i % 5;
+		【回答】0
+
+		質問２︓右の式を解釈してください：a=a>10?11:0;
+		【回答】等价于如下代码
+			 if(a>10) 
+				 a=11;
+			 else 
+				 a=0;
+
+		質問３︓変数値を比較する演算子は=or==？
+		【回答】==
+
+		質問４︓ブリアン（boolean）変数に設定可能の値は「?」「?」。
+		【回答】只能为「true」或「false」
+
+		 */
 	}
 
 }
--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,8 +3,32 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
+		/*
 
+		質問１︓System.out.println("this is line1\tthis line2");は何行出力しますか？
+		1. １行	√
+		2. ２行
 
+		質問２︓Stringを比較方法は？
+		1. ==
+		2. equals	√
+
+		質問３︓Float値⇒String文字列変更する場合、どうなメソッドを利用しますか？
+		1. String.valueOf("1.0F")	√
+		2. 1.0F + ""
+
+		質問４︓以下式の値を記載してください。
+		1. 1.0 + 2.0	【回答】3.0
+		2. 1.0 + "2.0"	【回答】1.02.0
+
+		質問５︓以下ステートメントの出力を記載してください。
+			public static void main(String[] args) {
+				int a = 10;
+				System.out.println("a");
+			}
+		【回答】a
+
+		 */
 	}
 
 }
