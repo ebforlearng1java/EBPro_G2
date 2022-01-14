bf0d55ffcf5d8cf8dd43367c87afbf95f3e547ef
rui <rui@192.168.3.34>
2022-01-14 18:24:00 +0900

    [EBHomework0102]　3回目　Mou

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -13,21 +13,20 @@ public class JavaHomeWork01 {
     System.out.println("Hello java");
 
     
-    //質問１︓以下変数nの値は︖
-   
-        // int i = 10;   a = 10
-        // int n = i % 5;  a = 0
+    //質問１︓以下変数nの値は︖  // int i = 10;  int n = i % 5; 
+      // A: a = 0
     
     //質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;  
-        //条件 a = a > 10;
-        //trueならば  : 10;
-        //falseならば : 0;
+      // A:
+      //条件 a = a > 10;
+      //trueならば  : 10;
+      //falseならば : 0;
        
     //質問３︓変数値を⽐較する演算⼦は = or == ? 
-    // ==
+      // A: ==
        
     //質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」  
-    // 「true」「false」
+      // A:「true」「false」
     
 		
 	}
