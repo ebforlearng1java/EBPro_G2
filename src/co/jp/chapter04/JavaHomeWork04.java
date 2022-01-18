597a9c2bc180429cff9031435f8214fcef0f2059
BruceTong36 <61258996+BruceTong36@users.noreply.github.com>
2022-01-18 15:18:54 +0900

    BruceTong-Homework04&05

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -17,11 +17,11 @@ public class JavaHomeWork04 {
 		
 		
 		//		問題４︓以下２重Foreachを理解してください。
-		//		int[][] array2d = new int[4][2];
-		//		for (int[] out : array2d) {
-		//			 for (int inner : out) {
-		//			 System.out.println(inner);
-		//			 } }
+//				int[][] array2d = new int[4][2];
+//				for (int[] out : array2d) {
+//					 for (int inner : out) {
+//					 System.out.println(inner);
+//					 } }
 		//		答え：2dの行列の全ての元素をプリントアウトしているコーディング。
 		//		0
 		//		0
@@ -36,42 +36,43 @@ public class JavaHomeWork04 {
 		//		問題５︓int[] array = new int[] {1, 3, 5, 7, 100, 0, 1}; 左の配列の最⼤値と最⼩桁値を 
 		//		System.out.println()。
 		//		答え：
-		//		int[] array = new int[] {1, 3, 5, 7, 100, 0, 1};
-		//		for(int i=0; i < (array.length-1); i++) {
-		//			for(int j=0; j < (array.length-1-i); j++){
-		//				if(array[j] < array[j+1]) {
-		//					int temp = array[j];
-		//					array[j] = array[j+1];
-		//					array[j+1] = temp;
-		//				}
-		//			}
-		//		}
-		//		System.out.printf("一番大きいのは%dです。\n", array[0]);
-		//		System.out.printf("一番小さいのは%dです。", array[array.length-1]);
+//				int[] array = new int[] {1, 3, 5, 7, 100, 0, 1};
+//				for(int i=0; i < (array.length-1); i++) {
+//					for(int j=0; j < (array.length-1-i); j++){
+//						if(array[j] < array[j+1]) {
+//							int temp = array[j];
+//							array[j] = array[j+1];
+//							array[j+1] = temp;
+//						}
+//					}
+//				}
+//				System.out.printf("一番大きいのは%dです。\n", array[0]);
+//				System.out.printf("一番小さいのは%dです。", array[array.length-1]);
 			
 		
 		//		問題６︓問題１の配列のSUMを求める。
 		//		答え：
-		//		int sum = 0;
-		//		for(i=0,  i < ,i++) {
-		//			sum += new int[i];
-		//		}
-		//		System.out.printf("この配列の和は%d\n", sum);
+//				int[] array = new int[4];
+//				int sum = 0;
+//				for(int i=0; i < array.length; i++) {
+//					sum += array[i];
+//				}
+//				System.out.printf("この配列の和は%d\n", sum);
 		
 		
 		//		問題７︓⾏列（２次元配列）の和と積を計算してください。
 		//		答え：
-		//		int acc = 0;
-		//		int mul = 1;
-		//		int[][] array2d = new int[3][4];
-		//		for(int i=0; i < array2d.length; i++) {
-		//			for(int j=0; j < array2d[0].length; j++) {
-		//				acc += array2d[i][j];
-		//				mul = mul * array2d[i][j];
-		//			}
-		//		}
-		//		System.out.printf("この配列の和は%d\n", acc);
-		//		System.out.printf("この配列の和は%d\n", mul);
+//				int acc = 0;
+//				int mul = 1;
+//				int[][] array2d = new int[3][4];
+//				for(int i=0; i < array2d.length; i++) {
+//					for(int j=0; j < array2d[0].length; j++) {
+//						acc += array2d[i][j];
+//						mul = mul * array2d[i][j];
+//					}
+//				}
+//				System.out.printf("この配列の和は%d\n", acc);
+//				System.out.printf("この配列の和は%d\n", mul);
 	}
 
 }
--- src/co/jp/chapter05/JavaHomeWork05.java
+++ src/co/jp/chapter05/JavaHomeWork05.java
@@ -4,7 +4,55 @@ public class JavaHomeWork05 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+//		設問１︓1から100までの偶数の和を求める(for + if)。
+//		答え：
+		int sum = 0;
+		for(int i =1; i<=100; i++) {
+			if(i % 2 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+		
+//		設問２︓5の階乗を求める(for)。
+//		答え：
+		int mul = 1;
+		for(int i=1; i<=5; i++) {
+			mul = mul * i;
+		}
+		System .out.println(mul);
+				
+//		質問３︓配列 {1, 2, 6, 7, 9, 6, 2, 1} の最⼤値、最⼩値、和、平均値を求める。
+//		答え：
+		int[] array = new int[] {1, 2, 6, 7, 9, 6, 2, 1};
+		for (int i = 0; i < array.length; i++) {
+			for(int j = 1; j < array.length-1-i;j++ ) {
+				if(array[i] < array[j]){
+					int temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;						
+				}
+			}
+		}
+		System.out.printf("配列の最大値は：%d\n", array[0]);
+		System.out.printf("配列の最小値は：%d\n", array[array.length-1]);
+		
+//		質問４︓質問３の配列に重複ある要素を探す（要素の表⽰次数集計必要なし）。
+//		答え：
+		
+		
+//		質問５︓質問３の配列に値「７」のインデックスを求む。
+//		答え：
+		
+//		質問６︓任意⾏列（２次元配列）の積を求める。
+//		答え：
+		
+//		質問７︓配列を昇順でソートする（java.util等メソッド利⽤禁⽌）。
+//		答え：
+		
+//		質問８︓LV10のパスカルの三⾓形を出⼒してください。
+//		答え：
+		
+		
 	}
-
 }
