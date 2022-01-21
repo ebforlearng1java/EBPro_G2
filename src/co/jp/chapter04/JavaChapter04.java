65bdf31bd5b17d8c57141aaa6bb45949b1ca7ffc
henan0829_homework <henan0829>
2022-01-21 22:44:46 +0900

    20211222 ri 初期設定

--- src/co/jp/chapter04/JavaChapter04.java
+++ src/co/jp/chapter04/JavaChapter04.java
@@ -4,8 +4,13 @@ public class JavaChapter04 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-
+		//問題１：[0,1,2,3]
+		//問題２:[4,5]
+		//問題３：[[0,1,2],[0,1,2]]
+		//問題4:
+		//問題5:
+		//問題6:
+		//問題7:
 	}
 
 }
