376a0e65b2ab620364f24710d467edb1daaf6222
52430 <52430@LAPTOP-O4VPII06>
2022-01-21 22:02:05 +0900

    assignment

--- src/co/jp/chapter02/JavaHomeWork02.java
+++ src/co/jp/chapter02/JavaHomeWork02.java
@@ -3,6 +3,36 @@ package co.jp.chapter02;
 public class JavaHomeWork02 {
 
 	public static void main(String[] args) {
+		//質問１︓ System.out.println(“this is line1\tthis line2“); は何⾏出⼒しますか︖ 
+		//1. １⾏ 2. ２⾏ 
+		//回答：\t tab缩进 \n 为换行
+		//質問２︓Stringを⽐較⽅法は︖ 1. == 2. equals
+		//回答：equals
+	    //質問3：Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか
+		//1. String.valueOf(“1.0F“) 2. 1.0F + ““ 
+		//回答：String.valueOf("1.0F")
+		//質問４︓以下式の値を記載してください。
+		//1. 1.0 + 2.0 2. 1.0 + “2.0“ 
+		//回答：1. 3.0    2.  1.02.0
+		//質問５︓以下ステートメントの出⼒を記載してください。 
+		//public static void main(String[] args) {
+		 int a = 10;
+		 System.out.println("a");
+		 //回答：a
+		
+
+	
+	
+
+		
+		
+		質問２︓Stringを⽐較⽅法は︖ 1. == 2. equals
+		質問３︓Float値⇒String⽂字列変更する場合、どうなメソッドを利⽤しますか 1. String.valueOf(“1.0F“) 2. 1.0F + ““ 
+		質問４︓以下式の値を記載してください。
+		1. 1.0 + 2.0 2. 1.0 + “2.0“ 
+		質問５︓以下ステートメントの出⼒を記載してください。 いいね 1 番に「いいね」しましょう ⽂字 説明 \' ' \" “ \\ \ \t Tab \n 改⾏ 1234 public static void main(String[] args) {
+				 int a = 10;
+				 System.out.println("a"); }
 
 
 	}
