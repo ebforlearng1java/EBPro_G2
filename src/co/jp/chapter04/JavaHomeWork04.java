d84994af5fe1e179440af9b5598d6e4c589828d5
52430 <52430@LAPTOP-O4VPII06>
2022-01-22 05:21:02 +0900

    a

--- src/co/jp/chapter04/JavaHomeWork04.java
+++ src/co/jp/chapter04/JavaHomeWork04.java
@@ -1,13 +1,80 @@
 package co.jp.chapter04;
 
+import java.lang.reflect.Array;
+
 public class JavaHomeWork04 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//問題１︓
+//		int [] array = new int[4]; 
+//		//各要素の値を記載してください。
+//		for(int i = 0;i < array.length;i++) {
+//		System.out.println(array[i]);
+//		}
+//		//结果为0 0 0 0
+		
+		
+//		
+////		問題２︓int[] array = new int[] {5, 6}; 各要素の値を記載してください。 
+//		int[] array = new int[] {5, 6};
+//		for(int i = 0;i < array.length;i++) {
+//			System.out.println(array[i]);
+//			}
+//		//结果为5 6
+//		
+		
+		
+////		問題３︓int[][] array = new int[2][3]; 各要素の値を記載してください。
+//		int[][] array = new int[2][3];
+//		//横三竖二的矩阵，没赋值所以为
+//		{0,0,0}
+//		{0,0,0}
+//		
+////		問題５︓int[] array = new int[] {1, 3, 5, 7, 100, 0, 1}; 
+////		左の配列の最⼤値と最⼩桁値を System.out.println()。
+//		int max = array[0];
+//		int min =array[0];
+//		int[] array = new int[] {1, 3, 5, 7, 100, 0, 1}; 
+//		for(int a = 0;a < array.length; a++) {
+//			if(max<array[a])
+//			{ max =array[a] ;
+//			  }
+//		}
+//		
+//			for(int b = 0;b< array.length; b++) {
+//				if(min>array[b])
+//				{ min =array[b] ;
+//				  }
+//				
+//			
+//			
+//		}
+//		System.out.println("最大値は"+max+","+"最小値は"+min+".");
+//		
+//	}
+//}
+		
+		
+//		
+////		問題６︓問題１の配列のSUMを求める。
+//		int sum = 0;
+//		int [] array = new int[4]; 
+//		for(int i = 0;i<array.length;i++) {
+//		    sum = sum + array[i];
+//		
+//			
+//		
+//	}
+//		System.out.println(sum);
+//	}
+//}
+		
+//		
+//		問題７︓⾏列（２次元配列）の和と積を計算してください。
 
-		System.out.println(String.valueOf("1.0F"));
-		System.out.println(String.valueOf(1.0F));
-		System.out.println(1.0F +"");
-	}
 
-}
+////		
+
+//}
+//}
