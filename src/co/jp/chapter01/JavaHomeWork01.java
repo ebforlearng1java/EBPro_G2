b77f39ae5fa4bee3d3fecd803ac6642a5e9f0c99
52430 <52430@LAPTOP-O4VPII06>
2022-01-21 22:03:33 +0900

    a

--- src/co/jp/chapter01/JavaHomeWork01.java
+++ src/co/jp/chapter01/JavaHomeWork01.java
@@ -3,10 +3,23 @@ package co.jp.chapter01;
 public class JavaHomeWork01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		//質問１︓以下変数nの値は︖
+		//int i = 10;
+		//int n = i%5;
+		//回答：
 
-
-		// dummy commit
+		int i = 10;
+		int n = i%5;
+		System.out.println(n);
+		//n的值为0. %取余符号 10/5 余零
+		//質問２︓ 右の式を解釈してください: a = a > 10 ? 11 : 0;
+		//回答：aは１０より大きいの場合、aに１１を代入する；逆に０に代入する。
+		
+		//質問３︓変数値を⽐較する演算⼦は = or == ?
+		//回答：比較する演算子は　==。　=　は代入演算子。
+		
+		//質問４︓ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+		//回答：　true and false.
 	}
 
 }
