141090f3c70f205446ca2c6b74e067096d8bea72
52430 <52430@LAPTOP-O4VPII06>
2022-01-22 05:21:36 +0900

    a

--- src/co/jp/chapter05/JavaHomeWork05.java
+++ src/co/jp/chapter05/JavaHomeWork05.java
@@ -4,6 +4,86 @@ public class JavaHomeWork05 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//設問１︓1から100までの偶数の和を求める(for + if)。 
+//		int sum = 0;
+//		for(int i = 0;i<=100;i++) {
+//			if(i%2==0) {
+//				sum = sum + i;
+//			}
+//		}
+//		System.out.println(sum);
+////		設問２︓5の階乗を求める(for)。
+//		int factorial = 1;//感觉不太合理
+//		for(int i = 5;i>0;i=i-1) {
+//		   factorial = factorial*i;
+//		
+//			
+//		}
+//		System.out.println(factorial);
+//		
+		
+		
+////		質問３︓配列 {1, 2, 6, 7, 9, 6, 2, 1} の最⼤値、最⼩値、和、平均値を求める。
+//		int[] array = new int[] { 1, 2, 6, 7, 9, 6, 2, 1 };
+//		int max = array[0];
+//		int min = array[0];
+//		float avg = 0;
+//		float sum = 0;
+//		for (int a = 0; a < array.length; a++) {
+//			if (max < array[a]) {
+//				max = array[a];
+//			}
+//		}
+//
+//		for (int b = 0; b < array.length; b++) {
+//			if (min > array[b]) {
+//				min = array[b];
+//			}
+//
+//		}
+//		for (int c = 0; c < array.length; c++) {
+//			sum = sum + array[c];
+//
+//		}
+//		avg = sum / array.length;
+//
+//		System.out.println("最大値は" + max + "," + "最小値は" + min + ",平均値は" + avg + ".");
+		
+////		質問４︓質問３の配列に重複ある要素を探す（要素の表⽰次数集計必要なし）。
+//		int[] array = new int[]{ 1, 2, 6, 7, 9, 6, 2, 1 };
+//		for (int i = 0; i < array.length; i++) {
+//			
+//				for(int j = i+1;j< array.length;j++) {
+//					if(array[i]==array[j]) {
+//						break; 
+//					}
+//				}
+//				 System.out.println(array[i]);
+//				
+//			}
+//			
+//		}
+//		有点小bug，不能保证不让后面相同数字再次出现。
+		   
+		//質問５︓質問３の配列に値「７」のインデックスを求む。 
+//		int[] array = new int[]{ 1, 2, 6, 7, 9, 6, 2, 1 };
+//		for (int i = 0; i < array.length; i++) {
+//			if(array[i] == 7) {
+//				System.out.println(i);
+//			}
+//		}
+//		
+		
+		
+//		質問６︓任意⾏列（２次元配列）の積を求める。
+		
+//		質問７︓配列を昇順でソートする（java.util等メソッド利⽤禁⽌）。
+		int[] array = new int[]{ 1, 2, 6, 7, 9, 6, 2, 1 };
+	    for (int i = 0; i < array.length; i++) {
+	    	
+	    }
+		
+//		質問８︓LV10のパスカルの三⾓形を出⼒してください。
 
 	}
 
