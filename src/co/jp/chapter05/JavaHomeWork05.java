2788ba7c4a31acadfd2bdd06e4cfe0e356436d33
henan0829_homework <henan0829>
2022-01-28 23:45:20 +0900

    homework05 rollback 20220128 sun

--- src/co/jp/chapter05/JavaHomeWork05.java
+++ src/co/jp/chapter05/JavaHomeWork05.java
@@ -3,8 +3,79 @@ package co.jp.chapter05;
 public class JavaHomeWork05 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+//	設問1回答:
+//  	int sum = 0;
+//		for(int i  = 0; i <= 100 ; i++) {
+//			if(i % 2 == 0) {
+//				sum += i;
+//				}
+//				}
+//		System.out.println(sum);
+//	質問2回答:
+//  	{
+//  		int kaijyou = 1;
+//		for(int i = 1; i <= 5; i++) {
+//				kaijyou = kaijyou*i;
+//		}
+//			System.out.println(kaijyou);
+//	}
+//	質問3回答:
+//  int[] array={1, 2, 6, 7, 9, 6, 2, 1};
+//	int max =array[0];
+//	int min = array[0];
+//	int sum=0;
+//		for(int i:array) {
+//		if(i>max) max=i;
+//		if(i<min) min=i;
+//		sum+=i;
+//	}
+//	double average =(double)sum/array.length;
+//		System.out.println("最大值："+max);
+//		System.out.println("最小値："+min);
+//		System.out.println("和："+sum);
+//		System.out.println("平均值："+average);
+//	質問4回答:
+// 		int[] array = new int[] {1, 2, 6, 7, 9, 6, 2, 1};
+//		for(int i = 0; i < array.length; i++) {
+//			for(int j = 1+i; j < array.length; j++) {
+//				if(array[i] == array[j]) {
+//					System.out.println("重複する数は " + array[j]);
+//				}
+//
+//			}
+//		}
+//	質問5回答:
+// 		int[] array = new int[] {1, 2, 6, 7, 9, 6, 2, 1};
+//		for(int i = 0; i < array.length; i++) {
+//			if(array[i] == 7) {
+//				System.out.println("「7」のインデックスは" + i);
+//			}
+//		}
+//	質問6回答:
+//      int[][] array1 = {{1,3},{5,7}};
+//		int[][] array2 ={{2,4},{6,8}};
+//		int[][] result=  new int[3][2];
 
-	}
+//		for(int i=0;i<array1.length;i++) {
+//			for(int j=0;j<array1[i].length;j++) {
+//				result[i][j]=array1[i][j]*array2[i][j];
+//				System.out.print(result[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+//	質問7回答:
+//		int[] array = new int[] {1, 2, 6, 7, 9, 6, 2, 1};
+//		 for (int i = 0; i < array.length; i++){
+//		        for (int j = i + 1; j < array.length; j++) {
+//		            int tmp = 0;
+//		            if (array[i] > array[j]) {
+//		                temp = array[i];
+//		                array[i] = array[j];
+//		                array[j] = temp;
+//		            }
+//		        }
+//		    }
+//		 for (int i = 0; i < array.length; i++){
+//		              System.out.print(array[i]+",");}
 
 }
