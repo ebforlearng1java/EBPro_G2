5971864e0a4a110bd6e205dc3475e6f9d1f6009d
mleng <mleng@MSI>
2022-01-28 14:22:16 +0900

    HW 8 2022/1/28
    Leng Mingbin

---
+++ src/co/jp/chapter08/JavaHomeWork08.java
@@ -0,0 +1,38 @@
+//質問１︓以下コードの出⼒するを回答してください。
+// True (文字列内容とアドレス両方とも同じです)  True(文字列内容は同じです)  False(変数nullStringはnullですが、emptyの値は空文字です。)
+
+//質問２︓以下コードの出⼒を回答してください。
+// False(objectを比較し、その結果アドレスが違います。)  True(Employee classはequalsをoverwriteして、アドレスじゃなく、文字列内容を比較し、同じ結果)
+
+
+//質問３︓以下コンパイルを修正してください。
+
+//public class Parent {
+//   public void hello() {
+//       System.out.println("parent method");
+//   }
+//}
+//
+
+//public class Child extends Parent {
+//   @Override // 親クラスの強制的にOverrideする意味（アノテーション）
+//   public void hello() {
+//       System.out.println("child method");
+//   }
+//}
+
+//public class Kicker {
+//   public static void print(Parent v) {
+//       v.hello();
+//   }
+//   public static void main(String...args) {
+//       print(new Parent());
+//       print(new Child());
+//   }
+//}
+
+
+
+
+
+
