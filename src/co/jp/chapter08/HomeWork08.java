d579bc9f1a8847d140650051f538f1a9db003b50
henan0829_homework <henan0829>
2022-01-29 01:02:38 +0900

    homework05 rollback 20220128 sun

---
+++ src/co/jp/chapter08/HomeWork08.java
@@ -0,0 +1,33 @@
+package co.jp.chapter05;
+public class JavaHomeWork08 {
+    public static void main(String[] args) {
+//        question1:　(1)true どちらも文字列から
+//                    (2)true 内容が一致
+//            　　　　　(3)false ""はnullと等しくない　
+//        　question2:(1)flase(2)true
+//          question3:
+//          // Parent.java
+//          public class Parent {
+//              public void hello() {
+//                  System.out.println("parent method");
+//              }
+//          }
+//        // Child.java
+//        public class Child extends Parent {
+//            @Override // 親クラスの強制的にOverrideする意味（アノテーション）
+//            public void hello() {
+//                System.out.println("child method");
+//            }
+//        }
+//        // Kicker.java
+//        public class Kicker {
+//            public static void print(Parent v) {
+//                v.hello();
+//            }
+//            public static void main(String...args) {
+//                print(new Parent());
+//                print(new Child());
+//            }
+//        }
+
+    }
