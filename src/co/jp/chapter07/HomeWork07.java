fb76725913dc8405b8a269a58fe6a8bb0504b053
henan0829_homework <henan0829>
2022-01-29 01:01:18 +0900

    homework05 rollback 20220128 sun

---
+++ src/co/jp/chapter07/HomeWork07.java
@@ -0,0 +1,56 @@
+package co.jp.chapter05;
+public class JavaHomeWork07 {
+    public static void main(String[] args) {
+//        question1: setageというメソッドがない
+//        question2:
+//        public static int max(int[] input) {
+//            int max =input[0];
+//            for(int i:input) {
+//                if(i>max) {
+//                    max=i;
+//                }
+//            }
+//            return max;
+//        }
+//
+//        public static int min(int[] input) {
+//            int min =input[0];
+//            for(int i:input) {
+//                if(i<min) {
+//                    min=i;
+//                }
+//            }
+//            return min;
+//        }
+//
+//        public static double ave(int[] input) {
+//            int sum =0;
+//            for(int i:input) {
+//                sum+=i;
+//            }
+//            return (double)sum/input.length;
+//        }
+//        public static void main(String[] args) {
+//
+//            int[] array= {1,4,2,7,6,3};
+
+//               System.out.println("max："+max(array));
+//			     System.out.println("min："+min(array));
+//			     System.out.println("ave："+ave(array));
+//            }
+//   quession2-2:
+//   public class Matrix{
+//      private final int[][]data;
+//      public Matrix(int[][]data){
+//			      this.data = data;
+//   }
+
+//       public Matrix add(Matrix b){
+//		Matrix c = new Matrix();
+//		c.data[0][0] = this.data[0][0] + b.data[0][0];
+//		c.data[0][1] = this.data[0][1] + b.data[0][1];
+//		c.data[1][0] = this.data[1][0] + b.data[1][0];
+//		c.data[1][1] = this.data[1][1] + b.data[1][1];
+//		return c;
+    }
+}
